8ae52a337f8879a2f3853f4fdbf4ed43d62ef406
bob <robertbalonek@gmail.com>
2021-04-23 14:55:15 -0700

    Fixed Route

--- server.js
+++ server.js
@@ -1,21 +1,20 @@
 const express = require("express");
 const cors = require("cors");
 const logger = require("morgan");
+const investmentsRoutes = require("./routes/investments");
 const db = require("./db/connection");
 const PORT = process.env.PORT || 3000;
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(logger("dev"));
 
-// const investmentsRoutes = require("./routes/investments");
-// app.use("/api", investmentsRoutes);
+app.use("/api", investmentsRoutes);
 
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
-app.listen(PORT, () => console.log(`Listeing on port: ${PORT}`));
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
 
-app.get("/", (req, res) => {
-  res.send("Root!");
-});
+app.get("/", (req, res) => res.send("This is root!"));
